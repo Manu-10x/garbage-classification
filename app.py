12fedbc587b3f177da7f71b3a00b7042bbcca07e
Manu-10x <143883124+Manu-10x@users.noreply.github.com>
2025-07-31 22:16:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,41 @@
-import os
-import requests
 import gradio as gr
 import tensorflow as tf
 import numpy as np
+import gdown
 from PIL import Image
+import os
 
-# Google Drive download link
-MODEL_URL = "https://drive.google.com/uc?id=10cfH3AeLud37TuQ-abrv67UPLPdFlF1e"
-MODEL_PATH = "efficientnetv2b2 (1).keras"
+# Model file name
+model_file = "efficientnetv2b2.keras"
 
-# Download model if not already present
-if not os.path.exists(MODEL_PATH):
-    print("Downloading model from Google Drive...")
-    response = requests.get(MODEL_URL)
-    with open(MODEL_PATH, "wb") as f:
-        f.write(response.content)
-    print("Download complete!")
+# Download from Google Drive if not present
+if not os.path.exists(model_file):
+    gdown.download(
+        "https://drive.google.com/uc?id=10cfH3AeLud37TuQ-abrv67UPLPdFlF1e", 
+        model_file, quiet=False
+    )
 
 # Load model
-model = tf.keras.models.load_model(MODEL_PATH)
+model = tf.keras.models.load_model(model_file)
 
-# Update with your actual class names
+# Class labels
 class_names = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
 
-def predict(image):
-    image = image.resize((124, 124))
-    image = np.array(image) / 255.0
-    image = np.expand_dims(image, axis=0)
-    pred = model.predict(image)[0]
-    return {class_names[i]: float(pred[i]) for i in range(len(class_names))}
+# Prediction function
+def predict(img):
+    img = img.resize((124, 124))
+    img = np.array(img) / 255.0
+    img = np.expand_dims(img, axis=0)
+    preds = model.predict(img)[0]
+    return {class_names[i]: float(preds[i]) for i in range(len(class_names))}
 
-interface = gr.Interface(
+# Gradio interface
+app = gr.Interface(
     fn=predict,
     inputs=gr.Image(type="pil"),
     outputs=gr.Label(num_top_classes=3),
-    title="Garbage Classifier",
-    description="Upload an image of garbage to classify its type."
+    title="Garbage Classification",
+    description="Upload an image to classify the type of waste."
 )
 
-interface.launch(server_name="0.0.0.0", server_port=8080)
+app.launch()
